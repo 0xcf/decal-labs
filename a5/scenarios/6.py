d271ef924aa5d7759f0e1c23365971bf251020dd
Tony Situ <c2.tonyc2@gmail.com>
2018-03-12 12:58:15 -0700

    Modify IO on scenario 6 and trimming basic ninja port to reduce question load

--- a5/scenarios/6.py
+++ a5/scenarios/6.py
@@ -7,8 +7,15 @@ routing_entries = util.get_default_routing_information()
 default_entry = next((e for e in routing_entries if util.is_default_gateway(e)), None)
 default_iface = default_entry.iface
 
-command = "sed -i 's/dhcp/manual/g' {iface_file}".format(iface_file=INTERFACES_FILE)
-util.run(command)
+lines = []
+with open(INTERFACES_FILE, "r+") as iface_file:
+    for line in iface_file:
+        if default_iface in line and "dhcp" in line:
+            lines.append(line.replace("dhcp", "manual"))
+        else:
+            lines.append(line)
+    iface_file.seek(0, 0)
+    iface_file.write(''.join(lines))
 
 command = "ip addr flush {iface}".format(iface=default_iface)
 util.run(command)
